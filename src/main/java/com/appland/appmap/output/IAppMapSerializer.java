3e9f6e2423c8e1c7856213108ab8955384d18242
Dustin Byrne <dustinsbyrne@gmail.com>
2019-10-18 16:58:23 -0400

    Remove dead code

--- src/main/java/com/appland/appmap/output/IAppMapSerializer.java
+++
@@ -1,9 +0,0 @@
-package com.appland.appmap.output;
-
-import com.appland.appmap.output.v1.CodeObject;
-import com.appland.appmap.output.v1.Event;
-import java.io.File;
-
-public interface IAppMapSerializer {
-  String serialize();
-}
