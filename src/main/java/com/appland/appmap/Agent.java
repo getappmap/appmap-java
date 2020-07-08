a297765876d10691eea94c01f90c41df0867d2cc
Dustin Byrne <dustinsbyrne@gmail.com>
2020-07-08 17:37:15 -0400

    System property abstraction
    Adds a single point for interfacing with and accessing system
    properties. Removes hard coded properties from existing code.

--- src/main/java/com/appland/appmap/Agent.java
+++ src/main/java/com/appland/appmap/Agent.java
@@ -1,6 +1,7 @@
 package com.appland.appmap;
 
 import com.appland.appmap.config.AppMapConfig;
+import com.appland.appmap.config.Properties;
 import com.appland.appmap.record.ActiveSessionException;
 import com.appland.appmap.record.Recorder;
 import com.appland.appmap.transform.ClassFileTransformer;
@@ -20,9 +21,6 @@ import java.lang.instrument.Instrumentation;
  * When the agent exits, any un-printed data will be written to the file <code>appmap.json</code>.
  */
 public class Agent {
-
-  private final static String DEFAULT_CONFIG_FILE = "appmap.yml";
-
   /**
    * premain is the entry point for the AppMap Java agent.
    * @param agentArgs agent options
@@ -30,25 +28,17 @@ public class Agent {
    * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/lang/instrument/package-summary.html">Package java.lang.instrument</a>
    */
   public static void premain(String agentArgs, Instrumentation inst) {
-    String appmapPath = System.getProperty("appmap.config.file");
-    if (appmapPath == null) {
-      appmapPath = DEFAULT_CONFIG_FILE;
+    final File dir = new File(Properties.OutputDirectory);
+    if (!dir.exists()) {
+      dir.mkdir();
     }
 
-    final String outputDirectory = System.getProperty("appmap.output.directory");
-    if (outputDirectory != null) {
-      final File dir = new File(outputDirectory);
-      if (!dir.exists()) {
-        dir.mkdir();
-      }
-    }
-
-    Logger.printf("AppMap: agent loaded using config %s\n", appmapPath);
+    Logger.printf("AppMap: agent loaded using config %s\n", Properties.ConfigFile);
 
     inst.addTransformer(new ClassFileTransformer());
 
-    if (AppMapConfig.load(new File(appmapPath)) == null) {
-      Logger.printf("AppMap: failed to load config %s\n", appmapPath);
+    if (AppMapConfig.load(new File(Properties.ConfigFile)) == null) {
+      Logger.printf("AppMap: failed to load config %s\n", Properties.ConfigFile);
       return;
     }
 
