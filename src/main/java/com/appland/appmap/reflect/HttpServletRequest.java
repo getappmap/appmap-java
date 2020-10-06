cd0e9b0f88ec52ad4ea6fe57621e049d4b99a2a4
Alan Potter <alanjpotter@gmail.com>
2020-10-06 17:22:23 -0400

    Don't cache methods found through reflection
    HttpServletRequest, HttpServletResponse, and FilterChain may created
    from objects of more than one type, so don't cache the methods we find
    through reflection.
    
    Also, use ReflectiveType.getMethod to find methods, to DRY up error
    handling.

--- src/main/java/com/appland/appmap/reflect/HttpServletRequest.java
+++ src/main/java/com/appland/appmap/reflect/HttpServletRequest.java
@@ -5,92 +5,41 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class HttpServletRequest extends ReflectiveType {
-  private static Method fnGetMethod;
-  private static Method fnGetRequestURI;
-  private static Method fnGetProtocol;
-  private static Method fnGetParameterMap;
+  private Method fnGetMethod;
+  private Method fnGetRequestURI;
+  private Method fnGetProtocol;
+  private Method fnGetParameterMap;
 
   public HttpServletRequest(Object self) {
     super(self);
 
-    if (fnGetMethod == null) {
-      try {
-        fnGetMethod = this.self.getClass().getMethod("getMethod");
-      } catch (Exception e) {
-        /* log an error */
-      }
-    }
-
-    if (fnGetRequestURI == null) {
-      try {
-        fnGetRequestURI = this.self.getClass().getMethod("getRequestURI");
-      } catch (Exception e) {
-        /* log an error */
-      }
-    }
-
-    if (fnGetProtocol == null) {
-      try {
-        fnGetProtocol = this.self.getClass().getMethod("getProtocol");
-      } catch (Exception e) {
-        /* log an error */
-      }
-    }
-
-    if (fnGetParameterMap == null) {
-      try {
-        fnGetParameterMap = this.self.getClass().getMethod("getParameterMap");
-      } catch (Exception e) {
-        /* log an error */
-      }
-    }
+    fnGetMethod = getMethod("getMethod");
+    fnGetRequestURI = getMethod("getRequestURI");
+    fnGetProtocol = getMethod("getProtocol");
+    fnGetParameterMap = getMethod("getParameterMap");
   }
 
   public String getMethod() {
-    if (fnGetMethod != null) {
-      try {
-        return (String) fnGetMethod.invoke(this.self);
-      } catch (Exception e) {
-        /* log an error */
-      }
-    }
-
-    return "";
+    return fnGetMethod != null?
+      (String)invoke(fnGetMethod)
+      : "";
   }
 
   public String getRequestURI() {
-    if (fnGetRequestURI != null) {
-      try {
-        return (String) fnGetRequestURI.invoke(this.self);
-      } catch (Exception e) {
-        /* log an error */
-      }
-    }
-
-    return "";
+    return fnGetRequestURI != null?
+      (String)invoke(fnGetRequestURI)
+      : "";
   }
 
   public String getProtocol() {
-    if (fnGetProtocol != null) {
-      try {
-        return (String) fnGetProtocol.invoke(this.self);
-      } catch (Exception e) {
-        /* log an error */
-      }
-    }
-
-    return "";
+    return fnGetProtocol != null?
+      (String)invoke(fnGetProtocol)
+      : "";
   }
 
   public Map<String, String[]> getParameterMap() {
-    if (fnGetProtocol != null) {
-      try {
-        return (Map<String, String[]>) fnGetParameterMap.invoke(this.self);
-      } catch (Exception e) {
-        /* log an error */
-      }
-    }
-
-    return new HashMap<String, String[]>();
+    return fnGetProtocol != null?
+      (Map<String, String[]>)invoke(fnGetParameterMap)
+      : new HashMap<String, String[]>();
   }
 }
