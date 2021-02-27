4c59f9dc42a61f835bcf668a666bdb2870cf8e96
Marcus Sanchez <m4rkuz@gmail.com>
2021-02-27 10:08:18 -0500

    Using parent event to create key for recording session.

--- src/main/java/com/appland/appmap/record/RecordingSessionGeneric.java
+++ src/main/java/com/appland/appmap/record/RecordingSessionGeneric.java
@@ -2,44 +2,57 @@ package com.appland.appmap.record;
 
 import com.appland.appmap.output.v1.CodeObject;
 import com.appland.appmap.output.v1.Event;
-import com.appland.appmap.util.Logger;
 
-import java.util.Vector;
 import java.util.HashSet;
+import java.util.Optional;
+import java.util.Vector;
 
 public class RecordingSessionGeneric implements IRecordingSession {
-  protected Vector<Event> events = new Vector<Event>();
-  protected HashSet<String> classReferences = new HashSet<>();
+    protected Vector<Event> events = new Vector<Event>();
+    protected HashSet<String> classReferences = new HashSet<>();
 
-  public synchronized void add(Event event) {
-    this.events.add(event);
+    public synchronized void add(Event event) {
+        this.events.add(event);
 
-    final String key = event.definedClass +
-      ":" + event.methodId +
-      ":" + event.isStatic + 
-      ":" + event.lineNumber;
-    this.classReferences.add(key);
-  }
-
-  public void start() {
-    throw new UnsupportedOperationException();
-  }
-
-  public String stop() {
-    throw new UnsupportedOperationException();
-  }
-
-  protected CodeObjectTree getClassMap() {
-    CodeObjectTree registeredObjects = Recorder.getInstance().getRegisteredObjects();
-    CodeObjectTree classMap = new CodeObjectTree();
-    for (String key: this.classReferences) {
-      String[] parts = key.split(":");
-     
-      CodeObject methodBranch = registeredObjects.getMethodBranch(parts[0], parts[1], Boolean.valueOf(parts[2]), Integer.valueOf(parts[3]));
-      if (methodBranch != null)
-        classMap.add(methodBranch);
+        if (event.parentId == null) {
+            this.classReferences.add(
+                    event.definedClass +
+                            ":" + event.methodId +
+                            ":" + event.isStatic +
+                            ":" + event.lineNumber);
+        } else {
+          Optional.ofNullable(events.stream()
+                    .filter(it -> it.id == event.parentId)
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Missing parent Event.")))
+                  .ifPresent( parentEvent ->
+                          this.classReferences.add(parentEvent.definedClass +
+                                  ":" + parentEvent.methodId +
+                                  ":" + parentEvent.isStatic +
+                                  ":" + parentEvent.lineNumber)
+                  );
+        }
     }
 
-    return classMap;
-  }
+    public void start() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String stop() {
+        throw new UnsupportedOperationException();
+    }
+
+    protected CodeObjectTree getClassMap() {
+        CodeObjectTree registeredObjects = Recorder.getInstance().getRegisteredObjects();
+        CodeObjectTree classMap = new CodeObjectTree();
+        for (String key : this.classReferences) {
+            String[] parts = key.split(":");
+
+            CodeObject methodBranch = registeredObjects.getMethodBranch(parts[0], parts[1], Boolean.valueOf(parts[2]), Integer.valueOf(parts[3]));
+            if (methodBranch != null)
+                classMap.add(methodBranch);
+        }
+
+        return classMap;
+    }
 }
