7136a4bb7e624c96e3564ab6f4006aa16a86f897
Marcus Sanchez <m4rkuz@gmail.com>
2021-03-06 15:52:16 -0500

    Fix Issue 56, added better way to extract the file path.
    Added tests for this.
    
    Removed src/main/java from file path calculation.

---
+++ src/test/java/com/appland/appmap/output/v1/testclasses/ExampleInnerClass.java
@@ -0,0 +1,11 @@
+package com.appland.appmap.output.v1.testclasses;
+
+import com.appland.appmap.ExampleClass;
+
+public class ExampleInnerClass extends ExampleClass {
+
+    private static final class StaticFinalInnerClass {
+
+    }
+
+}
