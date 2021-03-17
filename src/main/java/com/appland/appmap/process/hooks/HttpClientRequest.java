2131d822d8026900f00bacac7ccb8b146c65d464
Dustin Byrne <dustinsbyrne@gmail.com>
2021-03-17 13:50:04 -0400

    fix: disable http client requests (#60)
    Until the bootclass path hack can be removed, this must be disabled.

--- src/main/java/com/appland/appmap/process/hooks/HttpClientRequest.java
+++ src/main/java/com/appland/appmap/process/hooks/HttpClientRequest.java
@@ -11,21 +11,21 @@ import java.net.URL;
 @Unique("http_client_request")
 public class HttpClientRequest {
 
-    private static final Recorder recorder = Recorder.getInstance();
+  private static final Recorder recorder = Recorder.getInstance();
 
-    @HookClass(value = "java.net.HttpURLConnection")
-    public static void connect(Event event, HttpURLConnection httpURLConnection) {
-        //TODO: ReflectiveType can be used with HttpURLConnection
-        URL url = httpURLConnection.getURL();
-        event.setHttpClientRequest(httpURLConnection.getRequestMethod(), url.getHost(), url.getProtocol());
-        recorder.add(event);
-    }
+  // @HookClass(value = "java.net.HttpURLConnection")
+  public static void connect(Event event, HttpURLConnection httpURLConnection) {
+    //TODO: ReflectiveType can be used with HttpURLConnection
+    URL url = httpURLConnection.getURL();
+    event.setHttpClientRequest(httpURLConnection.getRequestMethod(), url.getHost(), url.getProtocol());
+    recorder.add(event);
+  }
 
-    @CallbackOn(MethodEvent.METHOD_RETURN)
-    @HookClass(value = "java.net.HttpURLConnection")
-    public static void connect(Event event, HttpURLConnection httpURLConnection, Object ret) throws IOException {
-        event.setHttpClientResponse(httpURLConnection.getResponseCode(), httpURLConnection.getContentType());
-        recorder.add(event);
-    }
+  // @CallbackOn(MethodEvent.METHOD_RETURN)
+  // @HookClass(value = "java.net.HttpURLConnection")
+  public static void connect(Event event, HttpURLConnection httpURLConnection, Object ret) throws IOException {
+    event.setHttpClientResponse(httpURLConnection.getResponseCode(), httpURLConnection.getContentType());
+    recorder.add(event);
+  }
 
 }
