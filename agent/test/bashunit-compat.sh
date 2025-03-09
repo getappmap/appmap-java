8d46dd578f6141d6c9219597193c35ddb10af0a1
Rafał Rzepecki <divided.mind@gmail.com>
2025-03-09 20:05:32 +0100

    Translate test-frameworks tests to bats

--- agent/test/bashunit-compat.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# This file is meant to contain any code required to use helper.bash functions
-# in a bashunit script.
-
-assert_equal() {
-  [[ ! -z "$3" ]] && assert_equals "$2" "$1" "$3" || assert_equals "$2" "$1"
-}
